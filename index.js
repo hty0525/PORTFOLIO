a6db09925dcd62de05e38e55f4f6a579e1ebc8ee
Taeyoung <hty3466@naver.com>
2021-12-10 19:23:49 +0900

    포폴수정

--- index.js
+++ index.js
@@ -17,4 +17,21 @@ const work = document.querySelector('#work');
 const contact = document.querySelector('#contact');
 
 
-console.log(home)
+
+
+
+console.log(home.getClientRects()[0].y)
+console.log(about.getClientRects()[0].y)
+console.log(work.getClientRects()[0].y)
+console.log(contact.getClientRects()[0].y)
+console.log(home.pageXOffset())
+document.addEventListener('scroll',()=>{
+  let windowY = window.scrollY;
+  if(windowY<about){
+    console.log('about')
+  }else if(windowY<work){
+    console.log('work')
+  }else if(windowY<contact){
+    console.log('contact')
+  }
+})
